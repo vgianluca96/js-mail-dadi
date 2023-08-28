5e05f3ad0c15d74d56a787622cf7ef4d72cfa416
vgianluca96 <gianluca.vallese@hotmail.com>
2023-08-28 15:43:40 +0200

    verzione 1
    impostata bozz del gioco dei dadi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,25 @@
 
-console.log('ciao');
+
+// Creazione Event Listener
+const btn = document.getElementById('btnDadi');
+btn.addEventListener("click", lancioDadi);
+
+function lancioDadi() {
+
+    const numLanci = document.getElementById('numLanci').value;
+    for (i = 1; i <= numLanci; i++) {
+        // Creazione numeri
+        let numGiocatore = Math.random();
+        let numComputer = Math.random();
+        // Arrotondamento numeri
+        numGiocatore = Math.round(numGiocatore*6);
+        numComputer = Math.round(numComputer*6);
+        //Confront risultati
+        if (numGiocatore > numComputer) {
+            console.log(numGiocatore,numComputer,'vince Giocatore');
+        } else if (numGiocatore < numComputer) {
+            console.log(numGiocatore,numComputer,'vince Computer');
+        } else console.log(numGiocatore,numComputer,'pari');
+    
+    }
+}
