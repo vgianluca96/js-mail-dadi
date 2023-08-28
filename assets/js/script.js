b098c32eeddd7b6642e31635312050bfcc91889c
vgianluca96 <gianluca.vallese@hotmail.com>
2023-08-28 16:13:06 +0200

    versione 2
    impostata logica per verifica mail

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,28 @@
 
 
-// Creazione Event Listener
-const btn = document.getElementById('btnDadi');
-btn.addEventListener("click", lancioDadi);
+// Creazione Event Listener dadi
+const btnDadi = document.getElementById('btnDadi');
+btnDadi.addEventListener("click", lancioDadi);
+
+// Creazione Event Listener mail
+const btnVerifica = document.getElementById('btnVerifica');
+btnVerifica.addEventListener("click", verificaMail);
+
+
 
 function lancioDadi() {
 
+    // Creazione variabile con numero lanci
     const numLanci = document.getElementById('numLanci').value;
-    for (i = 1; i <= numLanci; i++) {
+    // Ciclo che simula i vari lanci
+    for (let i = 1; i <= numLanci; i++) {
         // Creazione numeri
         let numGiocatore = Math.random();
         let numComputer = Math.random();
         // Arrotondamento numeri
         numGiocatore = Math.round(numGiocatore*6);
         numComputer = Math.round(numComputer*6);
-        //Confront risultati
+        //Confronto risultati
         if (numGiocatore > numComputer) {
             console.log(numGiocatore,numComputer,'vince Giocatore');
         } else if (numGiocatore < numComputer) {
@@ -22,4 +30,25 @@ function lancioDadi() {
         } else console.log(numGiocatore,numComputer,'pari');
     
     }
+}
+
+
+function verificaMail() {
+
+    // Creazione array con le mail in lista
+    const mailList = ['gianluca.vallese@hotmail.com', 'g.vallese@gmail.com', 'vallesegianluca@libero.it', 'gianlucav@yahoo.com'];
+    // Creazione variabile con la mail in input
+    const inputMail = document.getElementById('inputMail').value;
+    // Ciclo di confronto con tutte le mail    
+    for (let i = 0; i< mailList.length; i++) {
+        if (inputMail == mailList[i]) {
+            console.log('mail presente nella mailing list');
+        } else {
+            let a = mailList.length-1;
+            if (i == a) {
+                console.log('mail non presente');
+            }
+        }
+    }
+    
 }
